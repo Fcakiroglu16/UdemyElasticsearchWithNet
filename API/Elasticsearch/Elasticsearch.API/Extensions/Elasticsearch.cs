3dcef0bee067f071e8c019a94cb4175c21b4d7d6
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2023-05-24 00:50:05 +0300

    ElasticClient sınıfının oluşturulması

---
+++ API/Elasticsearch/Elasticsearch.API/Extensions/Elasticsearch.cs
@@ -0,0 +1,24 @@
+﻿using Elasticsearch.Net;
+using Nest;
+
+namespace Elasticsearch.API.Extensions
+{
+    public static class Elasticsearch
+    {
+
+        public static void AddElastic(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            var pool = new SingleNodeConnectionPool(new Uri(configuration.GetSection("Elastic")["Url"]!));
+
+
+            var settings = new ConnectionSettings(pool);
+
+
+            var client = new ElasticClient(settings);
+
+            services.AddSingleton(client);
+
+        }
+    }
+}
--- API/Elasticsearch/Elasticsearch.API/Program.cs
+++ API/Elasticsearch/Elasticsearch.API/Program.cs
@@ -1,3 +1,6 @@
+using Elasticsearch.Net;
+using Nest;
+using Elasticsearch.API.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddElastic(builder.Configuration);
 var app = builder.Build();
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
